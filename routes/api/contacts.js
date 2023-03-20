421bbd5f207c3523ebebfdcf720181b37a04f9a6
Valentina-K <kvalentyna@hotmail.com>
2023-03-20 10:47:05 +0200

    Made functions for access to data

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,17 +1,63 @@
 const express = require('express')
+const Joi = require('joi');
+const { listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+  updateContact,} = require('../../models/contacts');
 
 const router = express.Router()
-
+const schema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }),
+  phone: Joi.string().pattern(/^[0-9]+$/, { name: 'numbers'}),
+})
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const contacts = await listContacts();
+        res.json({
+            status: "success",
+            code: 200,
+            data: {
+                result: contacts
+            }
+        });
+  } catch (error) {
+    
+  }
+  
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const { contactId } = req.params;
+    const result = await getContactById(contactId);
+    if (!result) { }
+    res.json({ status: "success", code: 200, data: result});
+  } catch (error) {
+    
+  }
 })
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const {error} = schema.validate(req.body);
+        if(error){
+            error.status = 400;
+            throw error;
+    }
+    const result = await addContact(req.body);
+    res.status(201).json({
+            status: "success",
+            code: 201,
+            data: {
+                result
+            }
+        })
+  } catch (error) {
+    
+  }
+  
 })
 
 router.delete('/:contactId', async (req, res, next) => {
