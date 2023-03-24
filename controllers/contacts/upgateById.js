e933e9ba4b10b49e7ac15e06a61fa8dc5917ad58
Valentina-K <kvalentyna@hotmail.com>
2023-03-24 17:39:25 +0200

    Додала перевірку на пустий об'єкт, але не працює

--- controllers/contacts/upgateById.js
+++ controllers/contacts/upgateById.js
@@ -2,13 +2,17 @@ const operations = require('../../models/contacts');
 
 const updateContact = async (req, res, next) => {
   try {
-    const isEmpty = JSON.stringify(req.body) === '{}'
-    if (isEmpty) {
+    if (req.body.constructor===Object && Object.keys(req.body).length===0) {
       const error = new Error("missing fields");
       error.status = 400;
       throw error;
     }
-   
+    /* const isEmpty = !Object.keys(req.body).length;
+    if (isEmpty) {
+      const error = new Error("missing fields");
+      error.status = 400;
+      throw error;
+    } */
     const { contactId } = req.params;
     const result = await operations.updateContact(contactId, req.body);
      if (!result) {
