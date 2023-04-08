5e92a6c8c9e212d9a15a94315197506c3cf0b66a
Valentina-K <kvalentyna@hotmail.com>
2023-04-08 22:10:25 +0300

    add mongoose schema

--- models/contacts/getAll.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs/promises');
-const filePath = require("./filePath");
-
-const listContacts = async()=> {
-   const contacts = await fs.readFile(filePath);
-  return JSON.parse(contacts);
-}
-
-module.exports = listContacts;
--- server.js
+++ server.js
@@ -1,19 +1,18 @@
 const mongoose = require("mongoose");
 
-const app = require('./app')
+const app = require("./app");
 
-const {DB_HOST, PORT = 3000} = process.env;
-mongoose.set('strictQuery',true);
-//console.log(process.env)
+const { DB_HOST, PORT = 3000 } = process.env;
+mongoose.set("strictQuery", true);
+/* console.log(process.env) */
 
-mongoose.connect(DB_HOST)
-.then(()=>
-{
-  app.listen(PORT);
-  //console.log("success")
-})
-.catch(err=>
-  {
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(PORT);
+    console.log("Database connection successful");
+  })
+  .catch((err) => {
     console.log(err.message);
-    process.exit(1); //exix all processes
-})
+    process.exit(1); /* exix all processes */
+  });
