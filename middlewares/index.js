8c2864d0fca9ebedbd1b54e73bb7d01245026f1e
Valentina-K <kvalentyna@hotmail.com>
2023-04-16 21:47:58 +0300

    Made authenticate middleware

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,9 +1,11 @@
 const isEmptyBody = require("./isEmptyBody");
 const validation = require("./validation");
 const isValidId = require("./isValidId");
+const authenticate = require("./authenticate");
 
 module.exports = {
   isEmptyBody,
   validation,
   isValidId,
+  authenticate,
 };
